f7e324dc48dd3ac530b23f761252eb96a2a1219a
Dylan <yunxiyinzhe@gmail.com>
2013-09-05 23:38:47 +0800

    change UI of web translation

--- src/com/dylan/orzeye/WebTranslationActivity.java
+++ src/com/dylan/orzeye/WebTranslationActivity.java
@@ -1,33 +1,49 @@
 package com.dylan.orzeye;
 
+import com.fima.cardsui.objects.CardStack;
+import com.fima.cardsui.views.CardUI;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.TextView;
 
 public class WebTranslationActivity extends Activity {
 	
-	private TextView wordView;
-	private TextView phoneticView;
-	private TextView basicTanslationView;
-	private TextView webTanslationView;
+	private CardUI mCardView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_webtranslation);
-		
-		wordView = (TextView) findViewById(R.id.WordView);
-		phoneticView = (TextView) findViewById(R.id.PhoneticView);
-		basicTanslationView = (TextView) findViewById(R.id.BasicTranslationView);
-		webTanslationView = (TextView) findViewById(R.id.WebTranslationView);
-		
 		Intent intent = getIntent();
 		
-		wordView.setText(intent.getStringExtra("word"));
-		phoneticView.setText(intent.getStringExtra("phonetic"));
-		basicTanslationView.setText(intent.getStringExtra("basictanslation"));
-		webTanslationView.setText(intent.getStringExtra("webtanslation"));
+		mCardView = (CardUI) findViewById(R.id.cardsview);
+		mCardView.setSwipeable(true);
+
+		CardStack stack0 = new CardStack();
+		stack0.setTitle(intent.getStringExtra("word"));
+		mCardView.addStack(stack0);
+		
+		CardStack stack1 = new CardStack();
+		stack1.setTitle(intent.getStringExtra(" "));
+		mCardView.addStack(stack1);
+		mCardView.addCard(new CardView(getString(R.string.phonetic_title), 
+				intent.getStringExtra("phonetic"), "#e00707", "#e00707"));
+		
+		CardStack stack2 = new CardStack();
+		stack2.setTitle(intent.getStringExtra(" "));
+		mCardView.addStack(stack2);
+		mCardView.addCard(new CardView(getString(R.string.basictranslation_title), 
+				intent.getStringExtra("basictanslation"), "#f2a400", "#f2a400"));
+		
+		CardStack stack3 = new CardStack();
+		stack3.setTitle(intent.getStringExtra(" "));
+		mCardView.addStack(stack3);
+		mCardView.addCard(new CardView(getString(R.string.webtranslation_title), 
+				intent.getStringExtra("webtanslation"), "#9d36d0", "#9d36d0"));
+		
+		mCardView.refresh();
 	}
 }
